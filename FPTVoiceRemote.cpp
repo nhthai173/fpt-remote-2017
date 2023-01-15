c7c603e7a39b43ee8229a3cd0ded72ec308d6bb0
Thái <62926491+nhthai173@users.noreply.github.com>
2023-01-15 22:42:14 +0700

    Publish

---
+++ FPTVoiceRemote.cpp
@@ -0,0 +1,44 @@
+#include <stdint.h>
+class FPTRemote2017
+{
+    private:
+    public:
+        uint32_t number0Key = 0xFF00FF00;
+        uint32_t number1Key = 0xFE01FF00;
+        uint32_t number2Key = 0xFD02FF00;
+        uint32_t number3Key = 0xFC03FF00;
+        uint32_t number4Key = 0xFB04FF00;
+        uint32_t number5Key = 0xFA05FF00;
+        uint32_t number6Key = 0xF906FF00;
+        uint32_t number7Key = 0xF807FF00;
+        uint32_t number8Key = 0xF708FF00;
+        uint32_t number9Key = 0xF609FF00;
+        uint32_t okKey = 0xF10EFF00;
+        uint32_t backKey = 0xF00FFF00;
+        uint32_t homeKey = 0xEA15FF00;
+        uint32_t menuKey = 0xEB14FF00;
+        uint32_t tvKey = 0xE817FF00;
+        uint32_t filmKey = 0xE619FF00;
+        uint32_t setKey = 0xE718FF00;
+        uint32_t infoKey = 0xC33CFF00;
+        uint32_t redKey = 0xD728FF00;
+        uint32_t greenKey = 0xD629FF00;
+        uint32_t yellowKey = 0xD52AFF00;
+        uint32_t blueKey = 0xD42BFF00;
+        uint32_t accountKey = 0xC03FFF00;
+        uint32_t searchKey = 0xED12FF00;
+        uint32_t lastKey = 0xDD22FF00;
+        uint32_t favKey = 0xDC23FF00;
+        uint32_t channelupKey = 0xDF20FF00;
+        uint32_t channeldownKey = 0xDE21FF00;
+        uint32_t volumeupKey = 0xC13EFF00;
+        uint32_t volumedownKey = 0xC23DFF00;
+        uint32_t muteKey = 0xEE11FF00;
+        uint32_t powerKey = 0xEC13FF00;
+        uint32_t upKey = 0xF50AFF00;
+        uint32_t downKey = 0xF30CFF00;
+        uint32_t leftKey = 0xF20DFF00;
+        uint32_t rightKey = 0xF40BFF00;
+        uint32_t appKey = 0xE916FF00;
+        uint32_t cursorKey = 0xDA25FF00;
+};
---
+++ README.md
@@ -0,0 +1,7 @@
+# FPT Play box 2017 remote
+
+List of keys and key codes for FPT Play box 2017.
+
+<div style="display: flex; justify-content: center">
+    <img alt="FPT Play box 2017 remote" src="https://i.imgur.com/CxqprpE.png" style="max-height:400px">
+</div>
